08c9d993f7b88e945b5d41045747d41ddbedab76
dpoppe <dpoppe@harding.edu>
2022-10-18 10:57:35 -0500

    Add project files.

---
+++ ADONetDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADONetDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataSet musicDataSet = new DataSet();
+
+            // Setup the schema and the tables
+            musicDataSet.ReadXmlSchema("music.xsd");
+
+            // Read the XML data into the tables
+            musicDataSet.ReadXml("music.xml");
+
+            // After changes are made to the DataSet, save back to file
+            musicDataSet.WriteXml("music.xml");
+
+            MusicDataGrid.ItemsSource = musicDataSet.Tables["song"]?.DefaultView;
+        }
+    }
+}
